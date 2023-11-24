2f19c5e5641197d0de45e64b60707b4c2e30f290
NADIA LAGHRAOUI <81237363+NADIALAGHRAOUI@users.noreply.github.com>
2023-11-24 12:15:44 +0100

    Update file.py

--- file.py
+++ file.py
@@ -1,1 +1,8 @@
-
+def is_key_present(x):
+    # Check if 'x' is a key in the dictionary 'd'.
+    if x in d:
+        # If 'x' is present in 'd', print a message indicating that the key is present.
+        print('Key is present in the dictionary')
+    else:
+        # If 'x' is not present in 'd', print a message indicating that the key is not present.
+        print('Key is not present in the dictionary')
